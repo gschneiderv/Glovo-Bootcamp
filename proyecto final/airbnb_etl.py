6366145cebcd18b4c4196b4b8732cab082da81c8
gschneiderv <gschneiderv@gmail.com>
2023-02-13 21:47:55 +0100

    etl file connected to postgres

--- proyecto final/airbnb_etl.py
+++ proyecto final/airbnb_etl.py
@@ -9,7 +9,7 @@ df = pd.read_csv("airbnb-listings.csv", delimiter=";")
 
 
 #seleccionamos las columnas que nos servirán para realizar el análisis (Transform)
-df_madrid = df.filter(["Host Total Listing", "Street", "Neighbourhood Group Cleansed","City", "State", "Zipcode", "Smart Location","Country Code","Latitude", "Longitude", "Property Type", "Room Type","Accommodates", "Bathrooms", "Bedrooms", "Beds", "Bed Type", "Amenities", "Price", "Security Deposit", "Cleaning Fee", "Guests Included", "Extra People", "Minimum Nights", "Maximum Nights", "Number of Reviews", "Review Scores Rating", "Cancellation Policy", "Reviews per Month"],axis=1)
+df_madrid = df.filter(["Host Total Listing", "Street", "Neighbourhood Group Cleansed","City", "State", "Zipcode", "Smart Location","Country Code","Latitude", "Longitude", "Property Type", "Room Type","Accommodates", "Bathrooms", "Bedrooms", "Beds", "Bed Type", "Amenities","Square Feet", "Price", "Security Deposit", "Cleaning Fee", "Guests Included", "Extra People", "Minimum Nights", "Maximum Nights", "Number of Reviews", "Review Scores Rating", "Cancellation Policy", "Reviews per Month"],axis=1)
 
 #eliminamos las filas que NO tengan "City==Madrid and City==Delicias-Madrid", así me solo quedan las correspondientes a madrid
 #faltar ver como incluir las palabras "mal escritas",normalizar estos datos
@@ -18,5 +18,5 @@ df_airbnb_madrid = df_madrid[df_madrid["City"] == "Madrid"]
 
 #Lo cargamos a la base de datos (postgress)(load)
 
-engine = create_engine('postgresql://gaby:@localhost:5432/proyecto_final')
+engine = create_engine('postgresql://phmkeaeu:D-RiqEmcoViv8Sojo5L8ExxM9ufa0mkV@trumpet.db.elephantsql.com/phmkeaeu')
 df_airbnb_madrid.to_sql('airbnb_list', engine)
